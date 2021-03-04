7aef4e23f9d0b216c1974d9354165df810502492
unmaintain <61061617+unmaintain@users.noreply.github.com>
2021-03-04 21:15:44 +0700

    Update tmpl.js

--- tmpl.js
+++ tmpl.js
@@ -1,4 +1,4 @@
-/*! Tmpl.js v0.1.3 | felixexter | MIT | https://github.com/felixexter/tmpl.js */
+/*! Tmpl.js v0.1.4 | unmaintain | MIT | https://github.com/unmaintain/tmpl.js */
 (function () {
 	String.prototype.tmpl = function (data) {
 		return this.replace(/{{\s*?(\w*)\s*?}}/g, function(match, key, position) {
